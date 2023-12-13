7fb526266369ae72d8638bbcd7a886b7f9ab839c
hWeitian <weitian.huang@foodpanda.com>
2023-12-13 16:16:59 +0800

    Add custom font and typography theme

--- app/src/main/java/com/example/foodpanda_capstone/MainActivity.kt
+++ app/src/main/java/com/example/foodpanda_capstone/MainActivity.kt
@@ -3,14 +3,19 @@ package com.example.foodpanda_capstone
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.foodpanda_capstone.view.ui.theme.FoodpandaCapstoneTheme
+import com.example.foodpanda_capstone.view.ui.theme.Typography
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -28,10 +33,23 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+    Text(text = "Playlist Title Large", color = Color.Black, style = Typography.titleLarge)
+    Spacer(modifier = Modifier.size(10.dp))
+    Text(text = "Playlist Title Medium", color = Color.Black, style = Typography.titleMedium)
+    Spacer(modifier = Modifier.size(10.dp))
+    Text(text = "Playlist Title Small", color = Color.Black, style = Typography.titleSmall)
+    Spacer(modifier = Modifier.size(10.dp))
+    Text(text = "Playlist Headline Large", color = Color.Black, style = Typography.headlineLarge)
+    Spacer(modifier = Modifier.size(10.dp))
+    Text(text = "Playlist Headline Medium", color = Color.Black, style = Typography.headlineMedium)
+    Spacer(modifier = Modifier.size(10.dp))
+    Text(text = "Playlist Headline Small", color = Color.Black, style = Typography.headlineSmall)
+    Spacer(modifier = Modifier.size(10.dp))
+    Text(text = "Playlist Body Large", color = Color.Black, style = Typography.bodyLarge)
+    Spacer(modifier = Modifier.size(10.dp))
+    Text(text = "Playlist Body Medium", color = Color.Black, style = Typography.bodyMedium)
+    Spacer(modifier = Modifier.size(10.dp))
+    Text(text = "Playlist Body Small", color = Color.Black, style = Typography.bodySmall)
 }
 
 @Preview(showBackground = true)
--- app/src/main/java/com/example/foodpanda_capstone/view/ui/theme/Type.kt
+++ app/src/main/java/com/example/foodpanda_capstone/view/ui/theme/Type.kt
@@ -1,34 +1,83 @@
 package com.example.foodpanda_capstone.view.ui.theme
 
+import android.content.res.Resources
+import android.util.DisplayMetrics
 import androidx.compose.material3.Typography
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.example.foodpanda_capstone.R
 
-// Set of Material typography styles to start with
+val robotoFontFamily = FontFamily(
+    Font(R.font.roboto_900_black, FontWeight.Black),
+    Font(R.font.roboto_700_bold, FontWeight.Bold),
+    Font(R.font.roboto_400_regular, FontWeight.Normal),
+)
+
 val Typography = Typography(
-    bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 16.sp,
-        lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
-    )
-    /* Other default text styles to override
     titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
+        fontFamily = robotoFontFamily,
+        fontWeight = FontWeight.Bold,
+        fontSize = 24.sp,
         lineHeight = 28.sp,
-        letterSpacing = 0.sp
-    ),
-    labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
-        lineHeight = 16.sp,
         letterSpacing = 0.5.sp
-    )
-    */
+    ),
+    titleMedium = TextStyle(
+        fontFamily = robotoFontFamily,
+        fontWeight = FontWeight.Bold,
+        fontSize = 18.sp,
+        lineHeight = 21.sp,
+    ),
+    titleSmall = TextStyle(
+        fontFamily = robotoFontFamily,
+        fontWeight = FontWeight.Bold,
+        fontSize = 16.sp,
+        lineHeight = 19.sp,
+    ),
+    // Highlight base
+    headlineLarge = TextStyle(
+        fontFamily = robotoFontFamily,
+        fontWeight = FontWeight.Bold,
+        fontSize = 14.sp,
+        lineHeight = 16.sp,
+    ),
+    // Highlight small
+    headlineMedium = TextStyle(
+        fontFamily = robotoFontFamily,
+        fontWeight = FontWeight.Bold,
+        fontSize = 12.sp,
+        lineHeight = 14.sp,
+    ),
+    // Highlight xsmall
+    headlineSmall = TextStyle(
+        fontFamily = robotoFontFamily,
+        fontWeight = FontWeight.Bold,
+        fontSize = 10.sp,
+        lineHeight = 11.sp,
+    ),
+    // Body base
+    bodyLarge = TextStyle(
+        fontFamily = robotoFontFamily,
+        fontWeight = FontWeight.Normal,
+        fontSize = 14.sp,
+        lineHeight = 16.sp,
+    ),
+    // Body small
+    bodyMedium = TextStyle(
+        fontFamily = robotoFontFamily,
+        fontWeight = FontWeight.Normal,
+        fontSize = 12.sp,
+        lineHeight = 14.sp,
+    ),
+    // Body x small
+    bodySmall = TextStyle(
+        fontFamily = robotoFontFamily,
+        fontWeight = FontWeight.Normal,
+        fontSize = 10.sp,
+        lineHeight = 11.sp,
+    ),
 )
