65c93d06c483ada78fa397d5909e5deb231dd165
Pris <priscalimsz@gmail.com>
2023-12-19 17:22:16 +0800

    HomeScreenUI and SideNavBar

---
+++ app/src/main/java/com/example/foodpanda_capstone/view/ui/screen/MenuItems.kt
@@ -0,0 +1,10 @@
+package com.example.foodpanda_capstone.view.ui.screen
+
+import androidx.compose.ui.graphics.vector.ImageVector
+
+data class MenuItems(
+    val id: String,
+    val title: String,
+    val contentDescription: String,
+    val icon: ImageVector
+)
