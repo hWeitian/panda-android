7235324b7f554f2fac507a8db4060448dcdd1f6a
hWeitian <weitian.huang@foodpanda.com>
2023-12-21 10:50:17 +0800

    Update edit playlist layout

---
+++ app/src/main/java/com/example/foodpanda_capstone/view/ui/composable/BoxIconButton.kt
@@ -0,0 +1,71 @@
+package com.example.foodpanda_capstone.view.ui.composable
+
+import android.graphics.drawable.Icon
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.gestures.awaitFirstDown
+import androidx.compose.foundation.gestures.waitForUpOrCancellation
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Icon
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.composed
+import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import com.example.foodpanda_capstone.R
+import com.example.foodpanda_capstone.view.ui.theme.InteractionPrimary
+
+@Composable
+fun BoxIconButton(icon: @Composable () -> Unit, onClicked: () -> Unit) {
+    Box(
+        modifier = Modifier
+            .size(22.dp)
+            .bounceClick()
+            .clickable(
+                indication = null,
+                interactionSource = remember { MutableInteractionSource() } // This is mandatory
+            ) {
+                onClicked()
+            }
+    ) {
+        icon()
+    }
+}
+
+
+enum class ButtonState { Pressed, Idle }
+
+fun Modifier.bounceClick() = composed {
+    var buttonState by remember { mutableStateOf(ButtonState.Idle) }
+    val scale by animateFloatAsState(if (buttonState == ButtonState.Pressed) 0.70f else 1f, label = "")
+
+    this
+        .graphicsLayer {
+            scaleX = scale
+            scaleY = scale
+        }
+        .clickable(
+            interactionSource = remember { MutableInteractionSource() },
+            indication = null,
+            onClick = { }
+        )
+        .pointerInput(buttonState) {
+            awaitPointerEventScope {
+                buttonState = if (buttonState == ButtonState.Pressed) {
+                    waitForUpOrCancellation()
+                    ButtonState.Idle
+                } else {
+                    awaitFirstDown(false)
+                    ButtonState.Pressed
+                }
+            }
+        }
+}
--- app/src/main/java/com/example/foodpanda_capstone/view/ui/screen/EditPlaylistScreen.kt
+++ app/src/main/java/com/example/foodpanda_capstone/view/ui/screen/EditPlaylistScreen.kt
@@ -1,41 +1,61 @@
 package com.example.foodpanda_capstone.view.ui.screen
 
 import android.util.Log
+import androidx.compose.animation.core.InfiniteTransition
+import androidx.compose.animation.core.RepeatMode
+import androidx.compose.animation.core.animateFloat
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.infiniteRepeatable
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.gestures.awaitFirstDown
+import androidx.compose.foundation.gestures.waitForUpOrCancellation
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.composed
 import androidx.compose.ui.draw.drawBehind
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import com.example.foodpanda_capstone.R
 import com.example.foodpanda_capstone.model.FoodItem
+import com.example.foodpanda_capstone.view.ui.composable.BoxIconButton
 import com.example.foodpanda_capstone.view.ui.composable.FoodItemContainerCard
 import com.example.foodpanda_capstone.view.ui.composable.FoodItemDescriptionText
 import com.example.foodpanda_capstone.view.ui.composable.FoodItemNameText
 import com.example.foodpanda_capstone.view.ui.composable.ImageHolder
 import com.example.foodpanda_capstone.view.ui.composable.RestaurantSection
+import com.example.foodpanda_capstone.view.ui.composable.bounceClick
 import com.example.foodpanda_capstone.view.ui.theme.BrandDark
 import com.example.foodpanda_capstone.view.ui.theme.BrandSecondary
+import com.example.foodpanda_capstone.view.ui.theme.InteractionPrimary
 import com.example.foodpanda_capstone.view.ui.theme.Typography
 import com.example.foodpanda_capstone.viewmodel.PlaylistViewModel
 
@@ -44,10 +64,6 @@ fun EditPlaylistScreen(navController: NavController, viewModel: PlaylistViewMode
     // TODO: Add search bar
 
     val currentPlaylist by viewModel.currentPlaylist.collectAsState()
-
-    Log.i("WT", currentPlaylist.toString())
-
-//    Text(text = "Ehllo")
 
     Box(
         modifier = Modifier
@@ -97,39 +113,72 @@ fun EditableFoodItemContainer(foodItem: FoodItem) {
 @Composable
 fun EditableFoodItemContent(foodItem: FoodItem) {
     Row(
-        Modifier.fillMaxWidth(),
+        Modifier
+            .fillMaxWidth()
+            .height(90.dp),
         horizontalArrangement = Arrangement.SpaceBetween
     ) {
+        EditQuantityButtons(20) // TODO: Change to dynamic quantity
         Column(
             Modifier
-                .width(250.dp)
-                .height(80.dp),
+                .width(220.dp)
+                .fillMaxHeight(),
             verticalArrangement = Arrangement.SpaceBetween
         ) {
             Column {
                 FoodItemNameText(foodItem.name)
                 FoodItemDescriptionText(foodItem.description)
             }
-
-            Row(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(top = 5.dp),
-                horizontalArrangement = Arrangement.SpaceBetween
+            Text(
+                text = "S$ ${"%.2f".format(foodItem.price)}",
+                style = Typography.bodyMedium, fontWeight = FontWeight.Bold,
+                modifier = Modifier.padding(top = 5.dp)
             )
-            {
-                Text(
-                    text = "Qty: ${foodItem.quantity}",
-                    style = Typography.bodyMedium,
-                    color = BrandDark
-                )
-                Text(
-                    text = "S$ ${"%.2f".format(foodItem.price)}",
-                    style = Typography.bodyMedium
-                )
-            }
         }
         Spacer(modifier = Modifier.size(15.dp))
-        ImageHolder(imageUrl = foodItem.imageUrl, height = 80, description = foodItem.name)
+        ImageHolder(imageUrl = foodItem.imageUrl, height = 90, description = foodItem.name)
+    }
+}
+
+@Composable
+fun EditQuantityButtons(quantity: Int) {
+    Column(
+        modifier = Modifier
+            .fillMaxHeight()
+            .padding(end = 10.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.SpaceBetween
+    ) {
+        BoxIconButton({
+            Icon(
+                painter = painterResource(id = R.drawable.baseline_add_24_pink),
+                contentDescription = "Add Button",
+                tint = InteractionPrimary,
+            )
+        }, { Log.i("Panda", "Add btn clicked") }) // TODO: Add clicked logic
+
+        Card(
+            modifier = Modifier
+                .width(25.dp)
+                .height(25.dp),
+            shape = RoundedCornerShape(5.dp),
+            border = BorderStroke(1.dp, InteractionPrimary),
+            colors = CardDefaults.cardColors(containerColor = Color.White)
+        ) {
+            Column(
+                modifier = Modifier.fillMaxSize(),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Text(text = quantity.toString(), style = Typography.bodyMedium)
+            }
+        }
+        BoxIconButton({
+            Icon(
+                painter = painterResource(id = R.drawable.baseline_remove_24_pink),
+                contentDescription = "Remove Button",
+                tint = InteractionPrimary,
+            )
+        }, { Log.i("Panda", "Remove btn clicked") }) // TODO: Add clicked logic
     }
 }
