037716f0ea6f048c47c739677e89428af45402b9
Weitian Huang <weitian.huang@foodpanda.com>
2024-01-07 13:09:34 +0800

    Add error animation

---
+++ app/src/main/java/com/example/foodpanda_capstone/view/ui/composable/ErrorScreen.kt
@@ -0,0 +1,35 @@
+package com.example.foodpanda_capstone.view.ui.composable
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import com.airbnb.lottie.compose.LottieAnimation
+import com.airbnb.lottie.compose.LottieCompositionSpec
+import com.airbnb.lottie.compose.LottieConstants
+import com.airbnb.lottie.compose.rememberLottieComposition
+import com.example.foodpanda_capstone.R
+
+@Composable
+fun ErrorScreen () {
+    val composition by rememberLottieComposition(LottieCompositionSpec.RawRes(R.raw.error_animation_pink))
+
+    Surface(
+        modifier = Modifier.fillMaxSize(),
+    ) {
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            LottieAnimation(
+                composition = composition,
+                iterations = LottieConstants.IterateForever,
+            )
+        }
+    }
+}
